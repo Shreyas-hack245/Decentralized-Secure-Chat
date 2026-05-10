7bed11d647958062a9092c8ec2a01ca1d64f7e2b
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-05-11 00:05:37 +0530

    updated the app.jsx for chat ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,20 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div>
+
       <Navbar />
+
       <Hero />
+
       <Features />
+
+      <Chat />
+
     </div>
   );
 }
